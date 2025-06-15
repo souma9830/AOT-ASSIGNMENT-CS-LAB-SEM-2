28bc4434d581223443d5428169a4c590165cb3fc
Soumadeep Shee <prabirkumarshee8@gmail.com>
2025-06-15 15:47:27 +0530

    assignment

---
+++ Pattern/pattern1.c
@@ -0,0 +1,20 @@
+
+// *
+// **
+// ***
+// ****
+// ***** 
+
+#include<Stdio.h>
+int main(){
+    int n;
+    printf("Enter the row number");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=i;j++){
+        printf("*");
+      }
+      printf("\n");
+    }
+}
+
