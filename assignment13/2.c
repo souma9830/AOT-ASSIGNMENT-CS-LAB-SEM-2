b94d28485e3f7186e1d725316a577fd103c0a674
Soumadeep Shee <prabirkumarshee8@gmail.com>
2025-06-16 23:42:39 +0530

    major update

--- assignment13/2.c
+++ assignment13/2.c
@@ -1,24 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) {
-   if(argc != 4){
-      printf("Error: Invalid number of arguments\n");
-      return 1;
-   }
-
-   int num1 = atoi(argv[1]);
-   int num2 = atoi(argv[2]);
-   int num3 = atoi(argv[3]);
-
-   if(num1 > num2 && num1 > num3){
-      printf("The largest number is : %d\n", num1);
-   }
-   else if(num2 > num1 && num2 > num3){
-      printf("The largest number is : %d\n", num2);
-   }
-   else{
-      printf("The largest number is : %d\n", num3);
-   }
-   return 0;
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char *argv[]){
+    int a,b,c,max=0;
+    a=atoi(argv[1]);
+    b=atoi(argv[2]);
+    c=atoi(argv[3]);
+    if(a>b && a>c){
+        max=a;
+    }
+    else if(b>c && b>a){
+        max=b;
+    }
+    else{
+        max=c;
+    }
+    printf("Max vlaue is %d",max);
 }
